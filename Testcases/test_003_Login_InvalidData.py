e1ec1160099555909787db49885e89accf4f6215
Arati Rana Magar <aaratimgr090@gmail.com>
2026-01-14 14:28:59 +0545

    Refactor login test to use Firefox browser
    Updated the test to use Firefox directly and modified the login input.

--- Testcases/test_003_Login_InvalidData.py
+++ Testcases/test_003_Login_InvalidData.py
@@ -1,10 +1,11 @@
-from Base.InitiateDriver import startBrowser, closeBrowser
+#from Base.InitiateDriver import startBrowser, closeBrowser
+from selenium.webdriver import Firefox
 
 def test_InvalidateLogin():
-    driver=startBrowser()
-    driver.find_element('xpath',"//input[@name='email']").send_keys("==========")
+    driver=Firefox()
+    driver.get("https:/www.facebook.com/login")
+    driver.find_element('xpath',"//input[@name='email']").send_keys("Arati")
     driver.find_element('xpath',"//input[@name='pass']").send_keys("Arati@1234")
     driver.find_element('link text','Create new account').click()
-    closeBrowser()
-
-   
+    # closeBrowser()
+    driver.close()
